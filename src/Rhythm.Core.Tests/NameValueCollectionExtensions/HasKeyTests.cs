827181ed905946ed29cd416f2eee50e7f90a9b86
jamiepollock <jamie.pollock@rhythmagency.com>
2019-12-26 13:10:05 -0600

    Added initial NameValueCollectionExtensions with HasKey method and tests

---
+++ src/Rhythm.Core.Tests/NameValueCollectionExtensions/HasKeyTests.cs
@@ -0,0 +1,52 @@
+﻿namespace Rhythm.Core.Tests.NameValueCollectionExtensions
+{
+    using System.Collections.Specialized;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Rhythm.Core;
+
+    [TestClass]
+    public class HasKeyTests
+    {
+        [TestMethod]
+        public void Given_No_Matching_Keys_Should_Return_False()
+        {
+            // Arrange
+            var nvm = new NameValueCollection { { "key1", "value1" }, { "key2", "value2" }, { "key3", "value3" } };
+
+            // Act
+            var outcome = nvm.HasKey("key4");
+
+            // Assert
+            Assert.IsFalse(outcome);
+        }
+
+        [TestMethod]
+        public void Given_No_Keys_Should_Return_False()
+        {
+            // Arrange
+            var nvm = new NameValueCollection();
+
+            // Act
+            var outcome = nvm.HasKey("key1");
+
+            // Assert
+            Assert.IsFalse(outcome);
+        }
+
+        [TestMethod]
+        public void Given_An_Available_Matching_Key_Should_Return_True()
+        {
+            // Arrange
+            var nvc = new NameValueCollection() { { "key1", "value1" } };
+
+            // Act
+            var outcome = nvc.HasKey("key1");
+
+            // Assert
+            Assert.IsTrue(outcome);
+        }
+    }
+}
+
---
+++ src/Rhythm.Core/NameValueCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace Rhythm.Core
+{
+
+    // Namespaces.
+    using System.Collections.Generic;
+    using System.Collections.Specialized;
+    using System.Linq;
+
+    /// <summary>
+    /// A collection of extension methods to support the <see cref="NameValueCollection"/> interface.
+    /// </summary>
+    public static class NameValueCollectionExtensions
+    {
+        #region Properties
+        /// <summary>
+        /// Checks if the name value collection contains a key.
+        /// </summary>
+        /// <param name="nvc">The name value collection.</param>
+        /// <param name="key">The expecteed key.</param>
+        /// <returns>A <see cref="bool"/>.</returns>
+        public static bool HasKey(this NameValueCollection nvc, string key)
+        {
+            return nvc.AllKeys.Contains(key);
+        }
+        #endregion
+    }
+}
